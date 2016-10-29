acf454858ec7fc9370038eeb43e294ece5094b32
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2016-10-29 16:37:42 -0500

    [ Examples.hs ] Fixed typos.

--- Examples.hs
+++ Examples.hs
@@ -6,10 +6,10 @@ import LamTerm
 t1 :: Term
 t1 = App (Lam 'x' $ Lam 'y' $ Var 'x') (Lam 'z' $ Var 'z')
 
--- | This is the λ-term λxy.x(xy)
+-- | This is the λ-term λxy.x(xy).
 t2 :: Term
 t2 = Lam 'x' $ Lam 'y' $ App (Var 'x') (App (Var 'x') (Var 'y'))
 
--- | This is the λ-term λuv.u(uv)
+-- | This is the λ-term λuv.u(uv).
 t3 :: Term
 t3 = Lam 'u' $ Lam 'y' $ App (Var 'u') (App (Var 'u') (Var 'y'))
